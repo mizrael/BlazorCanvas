31bb8e84dfc51eba550a9aef8a0cd3aade53b10e
Davide Guida <mizrael@yahoo.it>
2020-06-29 00:28:17 -0400

    refactoring

---
+++ BlazorCanvas.Example3/GameTime.cs
@@ -0,0 +1,26 @@
+namespace BlazorCanvas.Example3
+{
+    public class GameTime
+    {
+        private float _totalTime = 0;
+
+        /// <summary>
+        /// total time elapsed since the beginning of the game
+        /// </summary>
+        public float TotalTime
+        {
+            get => _totalTime;
+            set
+            {
+                this.ElapsedTime = value - _totalTime;
+                _totalTime = value;
+
+            }
+        }
+
+        /// <summary>
+        /// time elapsed since last frame
+        /// </summary>
+        public float ElapsedTime { get; private set; }
+    }
+}
