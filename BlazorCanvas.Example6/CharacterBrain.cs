73e8273ed5fdf2a340786ea86294b87a9f3e378b
Davide Guida <mizrael@yahoo.it>
2020-07-02 15:53:43 -0400

    renamed component

---
+++ BlazorCanvas.Example6/CharacterBrain.cs
@@ -0,0 +1,12 @@
+using BlazorCanvas.Example6.Core;
+using BlazorCanvas.Example6.Core.Components;
+
+namespace BlazorCanvas.Example6
+{
+    public class LogoBrain : BaseComponent
+    {
+        public LogoBrain(AnimationsSet animationsSet, GameObject owner) : base(owner)
+        {
+        }
+    }
+}
