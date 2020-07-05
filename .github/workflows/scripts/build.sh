1bb36f9d1244c09cc81a79a8016ba21c5194f29e
Davide Guida <mizrael@yahoo.it>
2020-07-05 17:51:25 -0400

    first commit

---
+++ .github/workflows/scripts/build.sh
@@ -0,0 +1,10 @@
+projects=($(ls -1 -- **/*.csproj))
+for i in "${projects[@]}"
+do
+    filename=$(basename -- "$i")    
+    filename="${filename%.*}"
+    buildPath="build/$filename-t"    
+	dotnet publish --configuration Release $i --output $buildPath
+    mv $buildPath/wwwroot/ "build/$filename"    
+    rm -rf $buildPath
+done
