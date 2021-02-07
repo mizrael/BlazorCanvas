e3fdf4155c7f6d7fb4df5c0ba205099853be5993
David Guida <david.guida.03@gmail.com>
2021-02-06 21:28:30 -0500

    updated pipeline

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,19 +1,23 @@
-dotnet restore
-dotnet build -c Release --no-restore
+#!/bin/bash
 
 rm -rf build
+mkdir build
+cp readme.md build/readme.md
 
-projects=($(ls -1 -- **/*.csproj))
-for i in "${projects[@]}"
-do
-    filename=$(basename -- "$i")    
+publish(){
+    local projectName=$1
+    filename=$(basename -- "$projectName")    
     filename="${filename%.*}"
     buildPath="build/$filename-t"    
-	dotnet publish --configuration Release $i --output $buildPath --no-build --no-restore
+	dotnet publish --configuration Release $projectName --output $buildPath --no-build --no-restore
     mv $buildPath/wwwroot/ "build/$filename"        
     rm -rf $buildPath
 
     sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/BlazorCanvas\/$filename\/\" \/>/g" build/$filename/index.html
-done
+}
 
-cp readme.md build/readme.md
+dotnet restore
+dotnet build -c Release --no-restore
+
+projects=($(ls -1 -- **/*.csproj))
+for i in "${projects[@]}"; do publish "$i" & done
