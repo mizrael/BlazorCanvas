e9999d5af047ca8b7be0a64c470607eaa4d3c887
David Guida <david.guida.03@gmail.com>
2021-02-06 22:38:07 -0500

    ok let's try again

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -12,7 +12,10 @@ publish(){
     buildPath="build/$filename-tmp"    
 	dotnet publish --configuration Release $projectName --output $buildPath --no-build --no-restore
     
-    mv -v $buildPath/wwwroot/* $buildPath/wwwroot/.* "build/$filename"  
+    #mv -v $buildPath/wwwroot/* $buildPath/wwwroot/.* "build/$filename"  
+    mv $buildPath/wwwroot/ "build/$filename"
+    mv $buildPath/wwwroot/.* "build/$filename"  
+
     rm -rf $buildPath
 
     sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/BlazorCanvas\/$filename\/\" \/>/g" build/$filename/index.html
