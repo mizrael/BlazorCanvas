8eb83f9610cf487877d0c75de8988dc386738bb7
David Guida <david.guida.03@gmail.com>
2021-02-06 22:47:52 -0500

    ok maybe we're done

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -3,6 +3,7 @@
 rm -rf build
 mkdir build
 cp readme.md build/readme.md
+cp .nojekyll build/.nojekyll
 
 publish(){
     local projectName=$1
@@ -12,10 +13,7 @@ publish(){
     buildPath="build/$filename-tmp"    
 	dotnet publish --configuration Release $projectName --output $buildPath --no-build --no-restore
     
-    #mv -v $buildPath/wwwroot/* $buildPath/wwwroot/.* "build/$filename"  
-    mv $buildPath/wwwroot/ "build/$filename"
-    mv $buildPath/wwwroot/.* "build/$filename"  
-
+    mv $buildPath/wwwroot/ "build/$filename"    
     rm -rf $buildPath
 
     sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/BlazorCanvas\/$filename\/\" \/>/g" build/$filename/index.html
